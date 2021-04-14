20e7949bf1d2bc0e0f661fb97c6861851c517c16
wangting <1581404884@qq.com>
2021-04-15 01:02:43 +0800

    test

---
+++ Nodejs/2. 项目开发/test.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2021-04-15 01:01:42
+ * @LastEditTime: 2021-04-15 01:02:23
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \front-end-record\Nodejs\2. 项目开发\test.js
+ */
+let a = 1;
